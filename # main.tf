3d988dc9282119092f4a7c12b01170a6cc14a3e7
Hau Tran <30584990+tranhaubk0211@users.noreply.github.com>
2026-06-12 18:22:22 +0700

    Modify compute instance for cost estimation
    Updated the compute instance configuration to use a larger machine type and increased disk size for cost estimation.

--- # main.tf
+++ # main.tf
@@ -1,49 +1,16 @@
-# main.tf
-
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
-
 # ------------------------------------------------------------------------------
-# NETWORKING
-# ------------------------------------------------------------------------------
-resource "google_compute_network" "ai_vpc" {
-  name                    = "demo-vpc"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "ai_subnet" {
-  name          = "demo-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  network       = google_compute_network.ai_vpc.id
-  region        = var.region
-}
-
-# ------------------------------------------------------------------------------
-# HIGH-COST COMPUTE INSTANCE (To trigger Cost Estimation & Sentinel)
+# HEAVY STANDARD COMPUTE INSTANCE (Guaranteed to trigger Cost Estimation)
 # ------------------------------------------------------------------------------
 resource "google_compute_instance" "expensive_vm" {
   name         = "high-cost-demo-vm"
-  # Using an a2 machine type with attached GPUs ensures a high monthly baseline cost
-  machine_type = "a2-highgpu-1g" 
+  # n2-highcpu-96 is a massive 96 vCPU machine that forces a high, easy-to-estimate cost
+  machine_type = "n2-highcpu-96" 
   zone         = var.zone
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      size  = 200
+      size  = 500 # Larger disk size adds more measurable cost
       type  = "pd-ssd"
     }
   }
