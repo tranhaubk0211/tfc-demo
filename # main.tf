31d01d21d67fa2ae31aef455f6e94bfa4d53f22d
Hau Tran <30584990+tranhaubk0211@users.noreply.github.com>
2026-06-12 18:23:44 +0700

    Update main.tf for Google Cloud resources

--- # main.tf
+++ # main.tf
@@ -1,16 +1,48 @@
+# main.tf
+
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+# ------------------------------------------------------------------------------
+# NETWORKING (Must be included so the VM can reference them!)
+# ------------------------------------------------------------------------------
+resource "google_compute_network" "ai_vpc" {
+  name                    = "demo-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "ai_subnet" {
+  name          = "demo-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.ai_vpc.id
+  region        = var.region
+}
+
 # ------------------------------------------------------------------------------
 # HEAVY STANDARD COMPUTE INSTANCE (Guaranteed to trigger Cost Estimation)
 # ------------------------------------------------------------------------------
 resource "google_compute_instance" "expensive_vm" {
   name         = "high-cost-demo-vm"
-  # n2-highcpu-96 is a massive 96 vCPU machine that forces a high, easy-to-estimate cost
   machine_type = "n2-highcpu-96" 
   zone         = var.zone
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      size  = 500 # Larger disk size adds more measurable cost
+      size  = 500 
       type  = "pd-ssd"
     }
   }
